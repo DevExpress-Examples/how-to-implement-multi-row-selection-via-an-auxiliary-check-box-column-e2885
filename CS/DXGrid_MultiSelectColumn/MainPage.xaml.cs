b9f2f6caf6af58e55bccb93eaf13ac4d42301bfc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_MultiSelectColumn/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_MultiSelectColumn {
+    public partial class MainPage : UserControl {
+        List<TestData> list;
+        Dictionary<Guid, bool> selectedValues = new Dictionary<Guid, bool>();
+        public MainPage() {
+            InitializeComponent();
+            list = new List<TestData>();
+            for (int i = 0; i < 20; i++) {
+                list.Add(new TestData() { Id = Guid.NewGuid(), Number = i });
+            }
+            grid.DataSource = list;
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if (e.Column.FieldName == "Selected") {
+                Guid key = (Guid)e.GetListSourceFieldValue("Id");
+                if (e.IsGetData) {
+                    e.Value = GetIsSelected(key);
+                }
+                if (e.IsSetData) {
+                    SetIsSelected(key, (bool)e.Value);
+                }
+            }
+        }
+        bool GetIsSelected(Guid key) {
+            bool isSelected;
+            if (selectedValues.TryGetValue(key, out isSelected))
+                return isSelected;
+            return false;
+        }
+        void SetIsSelected(Guid key, bool value) {
+            if (value)
+                selectedValues[key] = value;
+            else
+                selectedValues.Remove(key);
+        }
+
+        private void BtnInvert_Click(object sender, RoutedEventArgs e) {
+            for (int i = 0; i < list.Count; i++) {
+                bool newIsSelected = !GetIsSelected(list[i].Id);
+                int rowHandle = grid.GetRowHandleByListIndex(i);
+                grid.SetCellValue(rowHandle, "Selected", newIsSelected);
+            }
+        }
+        private void BtnGetSelected_Click(object sender, RoutedEventArgs e) {
+            string selectedIds = string.Empty;
+            foreach (Guid key in selectedValues.Keys) {
+                selectedIds += string.Format("{0}\n", key);
+            }
+            string caption = string.Format("Selected rows (Total: {0})", selectedValues.Count);
+            MessageBox.Show(selectedIds, caption, MessageBoxButton.OK);
+        }
+    }
+
+    public class TestData {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement multi-row selection via an auxiliary check box column
+
+
+<p>This example shows how to create a Boolean unbound column and use it to select/unselect rows via check boxes.</p>
+
+<br/>
+
+
